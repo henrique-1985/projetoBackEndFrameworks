c1c72b67d0608acf80afcc626f3ffd1bc7da45c7
[Caroline Vitória Vieira de Abreu] <carolinevv01@gmail.com>
2025-05-18 09:29:55 -0300

    Ajustes de CSS

--- appmain.js
+++ appmain.js
@@ -147,41 +147,6 @@ app.get('/del-cadastro/:userId', function(req, res){
 
 });
 
-
-//modelo -------------
-
-app.get("/pagamento", function(req, res){
-	Pagamento.findAll({order: [['id', 'Asc']]}).then(function(pagamentos){
-		res.render('pagamento', {pagamentos: pagamentos});
-	})	
-});
-app.get("/cad-pagamento", function(req, res){
-	res.render("cad_pagamento");
-});
-app.post("/add-pagamento", function(req, res){
-	Pagamento.create({
-		nome: req.body.nome,
-		valor: req.body.valor
-	}).then(function(){
-		res.redirect('/pagamento')
-		//res.send("Pagamento cadastrado com sucesso")
-	}).catch(function(erro){
-		res.send("Erro ao realizar o cadastramento do pagamento!" + erro)
-	})
-});
-
-
-app.get('/del-pagamento/:id', function(req, res){
-	Pagamento.destroy({
-		where: {'id': req.params.id}
-	}).then(function(){
-		res.redirect('/pagamento')
-		//res.send("Pagamento excluído com sucesso!")
-	}).catch(function(erro){
-		res.send("Erro ao realizar a exclusão do Pagamento")
-	})
-})
-
 //Conexão
 
 app.listen(8085);
